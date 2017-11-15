49afe368af4f0b72fb32849b01f2576bc6fe6e3f
Kou Kamenashi <kamenashi0917@gmail.com>
2017-11-15 16:01:19 +0900

    refs #0004 create event function of backend
    * イベント管理機能
    ** 一覧画面：tr（行）内aタグクリック時挙動の実装漏れ＆バグを修正
    *** tr（行）をクリックしたら詳細画面にページ遷移するが、tr内a要素をクリックした際はa要素リンク挙動のみ実行し、詳細画面ページ遷移の挙動が重複するのを無視する処理のケア不足があったため。

--- web/js/event.js
+++ web/js/event.js
@@ -1,13 +1,14 @@
 // for event function
 (function () {
-  
+
   /**
    * tr(行)全体のクリッカブル&リンク先ページ遷移処理
    *****************************************************************************************************/
   document.querySelectorAll('tr[data-href]').forEach(function (tr) {
     tr.addEventListener('click', function(event) {
       // tr内のaタグのクリックは除く
-      if (event.target !== 'a') {
+      var a = tr.querySelector('a');
+      if (event.target !== a) {
         window.location.assign('/admin/event/detail.php?id=' + event.target.closest('tr').getAttribute('data-href'));
       }
     }, false);
