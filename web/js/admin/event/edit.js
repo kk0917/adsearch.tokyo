aada2a780273a5f1650d9027d99db80922f7a0ff
Kou Kamenashi <kamenashi0917@gmail.com>
2018-05-25 19:44:54 +0900

    refs #0004 create event function of backend
    * イベント編集機能
    ** サーバーバリデーションでのエラーメッセージ表示モーダル画面の実装

--- web/js/admin/event/edit.js
+++ web/js/admin/event/edit.js
@@ -6,23 +6,27 @@
   // Bootstrap3:modal不具合対応
     // 1. モーダル多層表示時に、オーバレイ->モーダル->オーバーレイ->モーダル->オーバーレイ...と交互に重なるようCSS:z-index値調整
   document.getElementById('eventEditArea').addEventListener('click', function(event) {
-    window.setTimeout(function () { // オーバーレイ要素が</body>直前に追加されるのを待つ
-      // Bootstrap3モーダル要素CSS:z-index初期値を設定
-      var defaultZindexValOfOverlay = 1040;
-      var defaultZindexValOfModal = 1050;
+    if (event.target.getAttribute('data-toggle') == 'modal') {
+      window.setTimeout(function () { // オーバーレイ要素が</body>直前に追加されるのを待つ
+        // Bootstrap3モーダル要素CSS:z-index初期値を設定
+        var defaultZindexValOfOverlay = 1040;
+        var defaultZindexValOfModal = 1050;
 
-      // イベント編集に関わるモーダル要素を取得
-      var overlay     = document.querySelectorAll('.modal-backdrop');
-      var modalWindow = document.querySelectorAll('#eventEditArea .modal');
+        // イベント編集に関わるモーダル要素を取得
+        var overlay     = document.querySelectorAll('.modal-backdrop');       // オーバーレイ要素は増減する
+        var modalWindow = document.querySelectorAll('#eventEditArea .modal'); // モーダル画面要素は増減しない
 
-      if (overlay.length == 2) { // 多層表示時に限り各モーダル要素のCSS:z-index値を調整
-        for (var i = 1; i < overlay.length; i++) { // 最下層のオーバーレイ＆モーダルより上のオーバーレイ＆モーダルに適用
-          var addValue = 10 * i
-          overlay[i].style.zIndex     = defaultZindexValOfOverlay + addValue;
-          modalWindow[i].style.zIndex = defaultZindexValOfModal + addValue;
+        if (overlay.length >= 2) { // 多層表示時に限り各モーダル要素のCSS:z-index値を調整
+          for (var i = 0; i < modalWindow.length; i++) {
+            var addValue = i == 0 ? 0 : 10 * (1 + i);
+
+            // 要素の増減有無に応じたz-indexの設定をする必要がある
+            if (i < overlay.length) { overlay[i].style.zIndex = defaultZindexValOfOverlay + addValue; }
+            if (modalWindow[i].classList.contains('in')) { modalWindow[i].style.zIndex = defaultZindexValOfModal + addValue; }
+          }
         }
-      }
-    }, 10);
+      }, 200); // モーダル要素が表示（classに in が追加される）されるのをこのミリ秒まで待つ必要がある
+    }
   }, false);
     // 2. 確認モーダル閉じた際に入力モーダルがスクロール不可になる問題を解決
   $('#eventEditConfirmModal').on('hidden.bs.modal', function () {
@@ -63,6 +67,11 @@
   //   document.querySelector('#eventEditConfirmModal #execUpdate').value = targetId;
   // }, false);
 
+  // バリデーションエラーメッセージモーダル画面を閉じる時に一緒に更新前確認モーダル画面も閉じる
+  document.getElementById('closeErrorModal').addEventListener('click', function() {
+    document.getElementById('backToInput').click();
+  }, false);
+
   // 更新実行イベント
   document.getElementById('execUpdate').addEventListener('click', function() {
     var updateEvent = document.editEventForm;
@@ -80,13 +89,24 @@
           window.alert('更新しました。');
           location.reload();
         } else {
-          window.alert(JSON.parse(ajax.responseText));
-
           // 更新確認モーダル画面（and オーバーレイ）を閉じる
-          document.getElementById('eventEditConfirmModal').classList.remove('in');
-          var overlays = document.body.querySelectorAll('.modal-backdrop');
-          var lastOverlay = overlays[(overlays.length - 1)]
-          lastOverlay.parentNode.removeChild(lastOverlay);
+          // document.getElementById('eventEditConfirmModal').classList.remove('in');
+          // var overlays = document.body.querySelectorAll('.modal-backdrop');
+          // var lastOverlay = overlays[(overlays.length - 1)]
+          // lastOverlay.parentNode.removeChild(lastOverlay);
+
+          // エラーメッセージをモーダル画面で表示
+            // エラーメッセージをエラーモーダル画面内に挿入
+          var errors         = JSON.parse(ajax.responseText);
+          var errorModalBody = document.querySelector('#eventEditValidationErrorModal .modal-body');
+          errorModalBody.innerHTML = ''; // bodyの中身をキレイにしておく
+          errors.forEach(function (error) {
+            var p = document.createElement('p');
+            p.textContent = '・' + error;
+            errorModalBody.appendChild(p);
+          });
+            // モーダル画面を表示させるために強制的にクリックイベントを発生させる
+          document.getElementById('triggerErrorModal').click();
         }
       }
     };
