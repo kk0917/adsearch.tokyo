63717ed07f7f4bc793b706903a98cf97c311eabe
Kou Kamenashi <kamenashi0917@gmail.com>
2018-06-04 21:13:23 +0900

    0008 frontend common
    * トップページの作成（途中）
    * リライト処理の変更（controllerパス変更）

--- src/AppBundle/Controller/index.php
+++ src/AppBundle/Controller/index.php
@@ -2,9 +2,18 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
+use AppBundle\Entity\EventRepository;
+
+$eventRepository = new EventRepository();
+
+// 開催中・開催予定イベントのレコードを取得
+$upcomingEvents  = $eventRepository->execute('SELECT_UPCOMING');
+
 $loader   = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/app/resources/views/');
 $twig     = new Twig_Environment($loader);
 
-echo $twig->render('index.html.twig');
+echo $twig->render('index.html.twig', [
+    'upcomingEvents' => $upcomingEvents,
+]);
 
 ?>
