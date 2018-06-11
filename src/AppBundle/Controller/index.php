6e648cfcfd102468cbfb3b3197e3a7c0c95bd4bf
Kou Kamenashi <kamenashi0917@gmail.com>
2018-06-11 23:12:03 +0900

    0008 frontend common
    * トップページの作成（途中）

--- src/AppBundle/Controller/index.php
+++ src/AppBundle/Controller/index.php
@@ -3,17 +3,22 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 use AppBundle\Entity\EventRepository;
-
-$eventRepository = new EventRepository();
+use AppBundle\Entity\PlaceRepository;
 
 // 開催中・開催予定イベントのレコードを取得
+$eventRepository = new EventRepository();
 $upcomingEvents  = $eventRepository->execute('SELECT_UPCOMING');
+
+// 会場情報のレコードを取得
+$placeRepository = new PlaceRepository();
+$places          = $placeRepository->execute('SELECT_ALL');
 
 $loader   = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/app/resources/views/');
 $twig     = new Twig_Environment($loader);
 
 echo $twig->render('index.html.twig', [
     'upcomingEvents' => $upcomingEvents,
+    'places'         => $places
 ]);
 
 ?>
